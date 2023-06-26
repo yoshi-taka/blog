6b933e5472177efa6716379d3174552b4a19cbec
yoshi-taka <smokeonthewater222@yahoo.co.jp>
2023-06-26 14:42:01 +0900

    vars

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Analytics from '../components/analytics'
 
 export default function Document() {
   return (
     <Html lang="ja">
       <Head />
+      <Analytics />
       <body>
         <Main />
         <NextScript />
